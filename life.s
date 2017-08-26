5aa6cd6b5406e6155d6eb74919870feb8aa03158
Emmet Murray <e.minor.murray@gmail.com>
2017-08-26 10:49:31 +1000

    Function now prints\!

--- life.s
+++ life.s
@@ -27,13 +27,15 @@ newBoard: .space 100
 
 	.data
 main_ret_save: .space 4
-hash: .asciiz "h"
+hash: .asciiz "#"
 dot: .asciiz "."
+newline: .asciiz "\n"
 	.text
 	.globl main
 main:
 	sw $ra, main_ret_save
 
+	jal copyBackAndShow
  #Your main program code goes here
 
 end_main:
@@ -50,15 +52,17 @@ neighbours: #Takes two int coordinates, returns the amount of neighbours in $a0
 
 copyBackAndShow: # Copies the new board over to the old, and prints it at the same time
 	# Load our board size constant into $t0 for use
-	li $t0 N
+	lw $t0 N
 	# Define our loop counters
 	li $t1 0
 	li $t2 0
 	#Incrementation rate
 	li $t7 1
 	outerCopy:
-		# if our outer counter is done, we exit
+		# if our outer counter is done (= N), we exit
 		beq $t1 $t0 endOuterCopy
+		# We need to reset out inner counter before continuing
+		li $t2 0
 		innerCopy:
 			# Jump to end if we are done
 			beq $t2 $t0 endInnerCopy
@@ -66,25 +70,30 @@ copyBackAndShow: # Copies the new board over to the old, and prints it at the sa
 			mul $t3 $t1 $t0
 			addu $t3 $t3 $t2
 			# Now $t3 has the byte we need. We can access it by doing board[$t3]
-			beqz newboard+$t3 caseDot
+			lb $t4 board($t3)
+			beqz $t4 caseDot
 			#print hash
 			la $a0 hash
-			li $v0 1
+			li $v0 4
 			syscall
 			# Increment and jump to the start of the loop
 			addu $t2 $t2 $t7
 			j innerCopy
-			# We print a dot
+		# We print a dot
 		caseDot:
 			#print dot
 			la $a0 dot
-			li $v0 1
+			li $v0 4 
 			syscall
 			# Increment and jump to the start of the loop
 			addu $t2 $t2 $t7
 			j innerCopy
 					
 		endInnerCopy:
+			#print newline
+			li $v0 4
+			la $a0 newline
+			syscall
 			#increment row counter and continue
 			addu $t1 $t1 $t7 
 			j outerCopy
