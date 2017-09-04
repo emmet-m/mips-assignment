86c8352dc9f572293f7f52165ce7b8244cba0820
Emmet Murray <e.minor.murray@gmail.com>
2017-09-04 23:36:35 +1000

    neighbours done

--- prog.s
+++ prog.s
@@ -28,13 +28,51 @@ end_main:
 
 #Takes two int coordinates, returns the amount of neighbours in $a0
 neighbours: 
-	li $t0, 0
+	# row is in $a0, column in $a1
+	# Save our result in $v0, load our constant N
+	li $v0, 0
+	lw $t9, N
+	# Load N^2 into a constant
+	mul $t7, $t9, $t9
+	# Load the 1D offset of board[x][y] into $t0 
+	mul $t0, $a0, $t9
+	add $t0, $t0, $a1
+	# Get our two loop counters
+	li $t1, -1
+	li $t2, -1
+	# Load our branching constant into $t8
+	li $t8, 1
 	outerNeighbours:
+		# If counter greater than 1, jump to the end
+		bgt $t1, $t8, outerNeighboursEnd
+		# Reset our counter
+		li $t2, -1
 		innerNeighbours:
-		innerNeighboursEnd:
-	outerNeighboursEnd:
+			# If counter greater than 1, jump to the end
+			bgt $t2, $t8, innerNeighboursEnd
+			# Recalculate offset into $t3
+			mul $t3, $t1, $t9 # +(-N || 0 || N)
+			add $t3, $t2, $t3 # +(-1 || 0 || 1)
+			add $t3, $t3, $t0 # (add offset to centre)
 
-	jr $ra
+			# Checks for a valid address
+			bltz $t3, caseFail # If index less than 0
+			bge $t3, $t7, caseFail # If index >= N^2, 
+			beq $t3 $t0 caseFail # If index is current index (we don't count ourself)
+			
+			# Now add the board inted $t3 into $t4
+			lb $t4, board($t3) # 1 or 0
+			$addi $v0, $v0, $t4
+		caseFail: # If a test for safety fails, jumps here
+			#incement and jump 
+			$addi $t2, $t2, 1
+			j innerNeighbours
+		# Increment outer counter and jump to start of loop
+		innerNeighboursEnd:
+			addi $t1, $t1, 1
+			j outerNeighbours
+	outerNeighboursEnd:		
+		jr $ra
 
 # Copies the new board over to the old, and prints it at the same time
 copyBackAndShow: 
